8111d96f837664b9a69112a842bad239803f5a6a
Василий <45902581+Vasneuro@users.noreply.github.com>
2022-06-10 02:44:41 +0300

    finally deep system

--- json_management.py
+++ json_management.py
@@ -6,55 +6,62 @@ def Error(ad_info=""):
 def help():
     info = '''
         JSON_manager allows you manage json files
-        file = JSON_manager()#here you have empty object
-        #but you already can work with json   
+        file = JSON_manager()# here you have empty object
+        # but you already can work with json   
         
-        #if you want open file
+        # if you want open file
         >>> file.read("name.json")
-        #or
+        # or
         >>> file = JSON_manager(name='name',read=True)
-        #if you want create file
+        # if you want create file
         >>> file.create("name.json")
-        #or
+        # or
         >>> file = JSON_manager(name='name',create=True)
-        #also if you don't want create json file at this moment
-        #you can set save=False
+        # also if you don't want create json file at this moment
+        # you can set save=False
         >>> test = JSON_manager(name='test.json',read=True)
         >>> test.data
         {'name': 'Petya', 'lvl': '100'}
         #
-        #change
+        # change
         #
         >>> test.change('name','Vasya')
         >>> test.data
         {'name': 'Vasya', 'lvl': '100'}
         #
-        #add
+        # add
         #
         >>> test.add('language','Pyton')
         >>> test.data
         {'name': 'Vasya', 'lvl': '100', 'language': 'Python'}
         #
-        #delete
+        # delete
         #
         >>> test.delete('language')
         >>> test.data
         {'name': 'Vasya', 'lvl': '100'}
         #
-        #open_dict
+        # open_dict
         #
         >>> test = JSON_manager(name='test.json',read=True)
         >>> test.data
         {'name': 'Petya', 'lvl': '100',
         'subjects': {'monday': 'math', 'tuesday': 'physics'}}
-        >>> i = test.open_dict('subjects')#it is instance of JSON_manager class
+        >>> i = test.open_dict('subjects')
+        # it is instance of JSON_manager class
         >>> i.data
         {'monday': 'math', 'tuesday': 'physics'}
         >>> i.change('monday','english')
         >>> i.data
         {'monday': 'english', 'tuesday': 'physics'}
-        >>> test.data#changes from i copies in original
+        >>> test.data
+        # changes from i copies in original
         {'name': 'Petya', 'lvl': '100', 'subjects': {'monday': 'english', 'tuesday': 'physics'}}
+        #
+        # save
+        #
+        # when you change JSON_manager any way it doesn't change json file
+        >>> file.save()
         '''
     print(info)
     
